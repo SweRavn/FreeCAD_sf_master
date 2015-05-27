9f277096ced7cfbcad8ca28c2473530b48bb2ca0
Robert <robert@rehammar.se>
2015-05-27 06:09:45 +0100

    Small rearrangement of CEM.py

--- src/Mod/Cem/CEM.py
+++ src/Mod/Cem/CEM.py
@@ -26,47 +26,10 @@ from datetime import datetime
 # Import the special CEM features:
 #import PML
 #import Source
-import lattice
+#import lattice
 #import Flux_region
 #import Far_field
 
-
-def export(objectslist,filename,nospline=False,lwPoly=False):
-    "called when freecad exports a file. If nospline=True, bsplines are exported as straight segs lwPoly=True for OpenSCAD DXF"
-    file = open(filename, "w")
-    file.write("""; Exported ctl-file from FreeCAD version %s.%s.%s.
-; This FreeCAD module was written by Robert Rehammar, robert@rehammar.se
-; File generated %s.
-; File units are FIXME
-
-"""%(FreeCAD.Version()[0], FreeCAD.Version()[1], FreeCAD.Version()[2], datetime.now().strftime('%c')))
-    
-    
-    # Export the geometry:
-    file.write("""
-; Exported geometry starts here: 
-
-(set! geometry
-    (list
-""")
-    for ob in objectslist:
-        if ob.isDerivedFrom("Part::Sphere"):
-            print_sphere(file, ob)
-        elif ob.isDerivedFrom("Part::Box"):
-            print_block(file, ob)
-        elif ob.isDerivedFrom("Part::Cylinder"):
-            #print_cylinder(file, ob)
-            pass
-    file.write("""
-    ) ; list
-) ; set! gemoetry
-; Exported geometry ends here.
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-""")
-    # Export the PML-layers:
-    # And so on...
-    
-    file.close()
 
 def print_sphere(file, so):
     """
@@ -111,7 +74,42 @@ def print_cylinder(file, so):
             (height %f)
         )\n"""%(so.Placement.Base[0],so.Placement.Base[1],so.Placement.Base[2],so.Radius,so.Height))
 
+def export(objectslist,filename,nospline=False,lwPoly=False):
+    "called when freecad exports a file. If nospline=True, bsplines are exported as straight segs lwPoly=True for OpenSCAD DXF"
+    file = open(filename, "w")
+    file.write("""; Exported ctl-file from FreeCAD version %s.%s.%s.
+; This FreeCAD module was written by Robert Rehammar, robert@rehammar.se
+; File generated %s.
+; File units are FIXME
 
+"""%(FreeCAD.Version()[0], FreeCAD.Version()[1], FreeCAD.Version()[2], datetime.now().strftime('%c')))
+    
+    
+    # Export the geometry:
+    file.write("""
+; Exported geometry starts here: 
+
+(set! geometry
+    (list
+""")
+    for ob in objectslist:
+        if ob.isDerivedFrom("Part::Sphere"):
+            print_sphere(file, ob)
+        elif ob.isDerivedFrom("Part::Box"):
+            print_block(file, ob)
+        elif ob.isDerivedFrom("Part::Cylinder"):
+            #print_cylinder(file, ob)
+            pass
+    file.write("""
+    ) ; list
+) ; set! gemoetry
+; Exported geometry ends here.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+""")
+    # Export the PML-layers:
+    # And so on...
+    
+    file.close()
 
 
 
